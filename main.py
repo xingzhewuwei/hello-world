03f85461c21511144c45fae5831bf66db7c23295
行者无畏 <79039532+xingzhewuwei@users.noreply.github.com>
2021-02-17 16:25:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
+"""
+创建一个提取CSDN上面优质文章并保存到本地的pdf文档，
+器主要目的就是全方位的学习Python的应用
+其中用到的模块如下：
+1、requests
+2、pdfkit
+3、pandas
+4、pdfmanager :多个文件的聚和
+5、os、：将一些临时文件或者目标文件进行文件夹的创建、删除、移动，
+6、BS4
+后续使用的库将会继续更新，
+目前全部使用函数来，后期会考虑使用类，讲函数全部封装在一起，
+先将大体的框架写出来，代码虽写的不是很好，但进过不断的优化，个人还是充满了信心！
 
-import sys 
-import pandas as pd
+
+"""
+import requests
+import pdfkit
+"""
+# 目前在编辑器中不需要初始化环境！
+def init_():
+  
+  pass
+
+"""
+def get_url(url):
+#  对得到的url进行分析，得到完整的页面并返回。
+  pass
+
+
+
+def down_load_pdf(html):
+#   将得到具有我们需要信息的html网页进行pdf格式的转换
+  
+  pass
+
+
+def filter_change():
+#   文件夹和路径的改变，
+  
+  pass
+
+
+
+
